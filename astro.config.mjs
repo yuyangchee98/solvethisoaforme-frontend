d194754f2ce1db15a6a8c8b3c2b2ff62b1f6aa7d
Yang <yuyangchee98@gmail.com>
2026-03-15 15:20:36 +0900

    url lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://solvethisoaforme.com',
+  site: 'https://solvethisoaforme.chyuang.com',
 
   vite: {
     plugins: [tailwindcss()]
